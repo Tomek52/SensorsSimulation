3924c3761a545b6fa2403d52f494c5d6119a5723
Tomek52 <tomek.szpytma@gmail.com>
2021-07-27 12:31:46 +0200

    added -h and --help

--- src/main.cpp
+++ src/main.cpp
@@ -5,23 +5,29 @@
 #include "Sensor.hpp"
 
 int main(int argc, char** argv) {
-    int numOfSensors = std::stoi(argv[1]);
-    int bufferSize = std::stoi(argv[2]);
+    if (argv[1] == "--help" or argv[1] == "-h" or argc != 3) {
+        std::cout << "Pass two params to prgram:\n"
+                  << "1. number of sensors.\n"
+                  << "2. size of buffer\n";
+    } else {
+        int numOfSensors = std::stoi(argv[1]);
+        int bufferSize = std::stoi(argv[2]);
 
-    std::vector<Sensor> sensors(numOfSensors, Sensor());
+        std::vector<Sensor> sensors(numOfSensors, Sensor());
 
-    CircBuffer circBuffer(bufferSize);
-    MainNode mainNode(circBuffer);
+        CircBuffer circBuffer(bufferSize);
+        MainNode mainNode(circBuffer);
 
-    auto mainNodePtr = std::make_shared<MainNode>(mainNode);
+        auto mainNodePtr = std::make_shared<MainNode>(mainNode);
 
-    for (auto& sensor : sensors) {
-        sensor.attach(mainNodePtr);
+        for (auto& sensor : sensors) {
+            sensor.attach(mainNodePtr);
+        }
+
+        for (auto& sensor : sensors) {
+            sensor.notify();
+        }
+        mainNode.printBuffer();
     }
-
-    for (auto& sensor : sensors) {
-        sensor.notify();
-    }
-    mainNode.printBuffer();
     return 0;
 }
